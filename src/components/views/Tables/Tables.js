ac8aaa2e7c3faf922e2e91108f27bd0a90984ba8
Szymon <sszulinski@gmail.com>
2021-05-23 17:41:18 +0200

    Add home view

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import 'date-fns';
 import styles from './Tables.module.scss';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Grid from '@material-ui/core/Grid';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
-const Tables = () => (
-  <Paper className={styles.component}>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Table 1</TableCell>
-          <TableCell>Table 2</TableCell>
-          <TableCell>Table 3</TableCell>
-          <TableCell>Table 4</TableCell>
-          <TableCell>Table 5</TableCell>
-          <TableCell>Table 6</TableCell>
-        </TableRow>
-      </TableHead>
-    </Table>
-  </Paper>
-);
+
+const demoContent = [
+  {id: '1', type: 'reservation', duration: 5},
+  {id: '2', type: 'reservation', duration: 2},
+  {id: '3', type: 'event', duration: 6},
+  {id: '4', type: 'reservation', duration: 3},
+  {id: '5', type: 'event', duration: 4},
+  {id: '6', type: 'reservation', duration: 5},
+];
+const Tables = () => {
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div className={styles.component}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid container justify="space-around">
+              </Grid>
+            </MuiPickersUtilsProvider>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Tables;
